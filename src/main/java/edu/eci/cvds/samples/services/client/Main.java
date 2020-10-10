4d09341c8e0564377905aec9ff62e00af219ee3e
Hogar <nikolas.bernal@mail.escuelaing.edu.co>
2020-10-09 22:54:32 -0500

    RegistroCliente completo, RegistroAlqulier Modificado

--- src/main/java/edu/eci/cvds/samples/services/client/Main.java
+++ src/main/java/edu/eci/cvds/samples/services/client/Main.java
@@ -11,10 +11,6 @@ import java.text.SimpleDateFormat;
 
 public class Main {
     public static void main(String args[]) throws SQLException, ExcepcionServiciosAlquiler, ParseException {
-        /* ServiciosAlquilerFactory.getInstance().getServiciosAlquilerTesting().registrarItem(new Item(new TipoItem(1, "Videojuego" ),1444,
-                "item99", "item99", new SimpleDateFormat("yyyy/MM/dd").parse("2019/09/28"),
-                99,"Digital","99")); */
-        System.out.println("Hola entre al main");
         System.out.println(ServiciosAlquilerFactory.getInstance().getServiciosAlquilerTesting().consultarTipoItem(90));
     }
 }
